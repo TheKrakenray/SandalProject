a6736d08a0629c07da00c76b90a9000a6c799d7b
unknown <andreangheleddu@gmail.com>
2024-09-25 15:34:34 +0200

    creazione dao classi

--- SandalProject/SandalProject/Models/Account.cs
+++ SandalProject/SandalProject/Models/Account.cs
@@ -1,8 +1,135 @@
-﻿using SandalProject.Utility;
+﻿using System.Net.Mail;
+using SandalProject.Utility;
+using System.Text.RegularExpressions;
 namespace SandalProject.Models
 {
     public class Account : Entity
     {
-        
+        public Account() { }
+        public Account(int id, string username, string email, string password, string ruolo, int pFeledelta): base(id)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            Ruolo = ruolo;
+            PFeledelta = pFeledelta;
+            
+        }
+        public Account(int id, string username, string email, string password, string ruolo, int pFeledelta, byte[] propic) : base(id)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            Ruolo = ruolo;
+            PFeledelta = pFeledelta;
+            Propic = propic;
+        }
+
+
+        #region Properties
+        private string? _username;
+        public string? Username 
+        {
+            get => _username;
+            set
+            {
+                if(value != null)
+                {
+                    Regex userRGX = new Regex(@"^[a-zA-Z0-9_-]{1,15}$");
+                    //regex per l'user name può contenere caratteri normali e "-" o "_" , deve essere lungo fra 1 e 15 caratteri 
+                    if (userRGX.Match(value).Success)
+                        _username = value;
+                    else
+                        throw new ArgumentException("Invalid email format");
+                }
+                else
+                {
+                    _username = "null";
+                }
+            }
+        }
+
+        private string? _email;
+        public string? Email 
+        {
+            get => _email;
+            set
+            {
+                if(value!=null)
+                {
+                    Regex emailRGX = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                    //Regex per l'email: deve contenere un "@" può contenere per forza e massimo 2 punti uno all'inizio e uno dopo la chiocciola
+                    //quindi abc.bla@email.com
+                    if (emailRGX.Match(value).Success)
+                        _email = value;
+                    else
+                        throw new ArgumentException("Invalid email format");
+                }
+                else
+                {
+                    _email = "null";
+                }
+            }
+        }
+
+        private string? _password;
+        public string? Password 
+        {
+            get => _password;
+            set
+            {
+                if(value == null)
+                {
+                    _password = "null";
+                }
+                else
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        private string? _ruolo;
+        public string? Ruolo 
+        {
+            get => _ruolo;
+            set
+            {
+                if (value==null || value.ToLower() != "admin")
+                    _ruolo = "user";
+                else
+                    _ruolo = value;
+            }
+        }
+
+        private int? _pFedelta; 
+        public int? PFeledelta 
+        {   get => _pFedelta; 
+            set
+            {
+                if (value != null)
+                {
+                    if(value < 0)
+                    {
+                        _pFedelta = 0;
+                    }
+                    else if(value > 500)
+                    {
+                        _pFedelta = 500;
+                    }
+                    else
+                    {
+                        _pFedelta = (int)value;
+                    }
+                }
+                else
+                {
+                    _pFedelta = 0;
+                }
+            }
+        }
+
+        public byte[]? Propic { get; set; }
+        #endregion
     }
 }
--- SandalProject/SandalProject/Models/DAOAccount.cs
+++ SandalProject/SandalProject/Models/DAOAccount.cs
@@ -1,32 +1,170 @@
 ﻿using SandalProject.Utility;
+using System.Runtime.CompilerServices;
 
 namespace SandalProject.Models
 {
     public class DAOAccount : IDAO
     {
+        private Database db;
+
+        #region singleton
+        private static DAOAccount instance = null;
+
+        private DAOAccount()
+        {
+            db = Database.GetInstance();
+        }
+
+        public static DAOAccount GetInstance()
+        {
+            if (instance == null)
+                instance = new DAOAccount();
+            return instance;
+        }
+        #endregion
+
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            bool del = false;
+            try
+            {
+                del = db.Update($"delete from Account where id = {id}");
+            }
+            catch
+            {
+                throw new ArgumentException("Errore rimozione account");
+            }
+            return del;
+        }
+
+
+        public Entity Find(string username)
+        {
+            Dictionary<string, string> riga=null;
+            try
+            {
+                riga = db.ReadOne($"select * from account where username = {username}");
+            }
+            catch
+            {
+                throw new ArgumentException("Errore account non esistente");
+            }
+
+            Account a = new();
+            if(riga != null)
+            {
+                a.FromDictionary(riga);
+            }
+            return a;
         }
 
         public Entity Find(int id)
         {
-            throw new NotImplementedException();
+            Dictionary<string, string> riga = null;
+            try
+            {
+                riga = db.ReadOne($"select * from account where id = {id}");
+            }
+            catch
+            {
+                throw new ArgumentException("Errore account non esistente");
+            }
+            Account account = new();
+            if(riga != null)
+                account.FromDictionary(riga);
+            return account;
         }
 
         public bool Insert(Entity e)
         {
-            throw new NotImplementedException();
+            Account a = (Account)e;
+            bool ins = false;
+            try
+            {
+                ins = db.Update($"Insert into Account" +
+                             $"Propic, Username, Email, Psw, Ruolo, PFedelta" +
+                             $"Values" +
+                             $"('{a.Propic}'," +
+                             $"'{(a.Username == null ? "null" : a.Username.Replace("'", "''"))}'," +
+                             $"'{(a.Email == null ? "null" : a.Email.Replace("'", "''"))}'," +
+                             $"'{(a.Password == null ? "null" : a.Password.Replace("'", "''"))}'," +
+                             $"'{(a.Ruolo == null ? "null" : a.Ruolo.Replace("'", "''"))},{a.PFeledelta}')");
+            }
+            catch
+            {
+                throw new ArgumentException("Errore nell'inserimento");
+            }
+
+            return ins;
         }
 
-        public List<Entity> ReadAll()
+        public List<Entity> ReadAll() 
         {
-            throw new NotImplementedException();
+            throw new ArgumentException("funzione deprecata");
+        }
+        public List<Entity> ReadAll(string AdminPsw)
+        {
+            List<Entity> lista = new();
+
+            List<Dictionary<string,string>> righe = null;
+            try
+            {
+                if(Valida("admin",AdminPsw))
+                    righe = db.Read("Select Account.username,Account.email,Account.ruolo,Account.Pfedelta from Account");
+            }
+            catch
+            {
+                throw new ArgumentException("errore nella lettura");
+            }
+
+            if(righe!=null)
+            foreach(var r in righe)
+            {
+                Account a = new();
+                a.FromDictionary(r);
+                lista.Add(a);
+            }
+
+            return lista;
         }
 
         public bool Update(Entity e)
         {
-            throw new NotImplementedException();
+            Account a = new();
+            bool upd = false;
+            try
+            {
+                upd = db.Update($"UPDATE account SET " +
+                                 $"Username = '{(a.Username == null ? "null" : a.Username.Replace("'", "''"))}' " +
+                                 $"Email = '{(a.Email == null ? "null" : a.Email.Replace("'","''"))}'," +
+                                 $"Password = 'HASHBYTES('SHA2_512','{(a.Password == null ? "null" : a.Password.Replace("'", "''"))}')," +
+                                 $"Ruolo = '{(a.Ruolo == null ? "null" : a.Ruolo.Replace("'", "''"))}'," +
+                                 $"PFedelta = {a.PFeledelta}," +
+                                 $"WHERE id = {a.Id}");
+            }
+            catch
+            {
+                throw new ArgumentException("Errore modifica account");
+            }
+            return upd;
+        }
+
+        public bool Valida(string username_email, string password)
+        {
+            Dictionary<string, string> riga = null;
+            try
+            {
+                riga = db.ReadOne($"SELECT account.username FROM account WHERE (username = '{(username_email.Replace("'", "''"))}' OR email = '{(username_email.Replace("'", "''"))}') AND passw = HASHBYTES('SHA2_512','{(password.Replace("'", "''"))}');");
+            }
+            catch
+            {
+                throw new ArgumentException("errore nella validazione");
+            }
+
+            if (riga != null)
+                return true;
+            else
+                return false;
         }
     }
 }
