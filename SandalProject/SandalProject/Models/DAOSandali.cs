cacd49960b0e3d74fa2ea12668514fd31b994855
FrancescoP5 <francesco.panico333@gmail.com>
2024-10-04 17:10:29 +0200

    visualizzazione del prodotto

--- SandalProject/SandalProject/Models/DAOSandali.cs
+++ SandalProject/SandalProject/Models/DAOSandali.cs
@@ -329,7 +329,7 @@ namespace SandalProject.Models
 
             try
             {
-                righe = db.Read($"SELECT * FROM Sandali WHERE LEFT(sku, LEN(sku) - 4) = '{sku}' Order by case When id = 5 then 0 else 1 END;");
+                righe = db.Read($"SELECT * FROM Sandali WHERE Left(sku,Len(sku)-4) = '{sku}' Order by case When id = {id} then 0 else 1 END;");
 
             }
             catch
